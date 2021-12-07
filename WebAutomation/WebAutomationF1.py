c5b61c0a0e1a06572e1f48be559320da0228e7dc
mirvakas <36821997+mirvakas@users.noreply.github.com>
2021-12-08 02:27:28 +0530

    08/12/2021 - Initial Code Commit (Web Driver Connect)

--- WebAutomation/WebAutomationF1.py
+++ WebAutomation/WebAutomationF1.py
@@ -1,6 +1,12 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# COMMENT
-# Comment 2
+driver = webdriver.Chrome()
+driver.get("https://www.python.org")
+print(driver.title)
+search_bar = driver.find_element("q")
+search_bar.clear()
+search_bar.send_keys("getting started with python")
+search_bar.send_keys(Keys.RETURN)
+print(driver.current_url)
+driver.close()
