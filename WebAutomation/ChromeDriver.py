ddd485c130eab5b9232dcb698791ff58f9fc9ebf
Vikaus Mir <mirvakas@gmail.com>
2023-01-22 18:25:32 +0530

    1/22/2023 - Posting Code Online

---
+++ WebAutomation/ChromeDriver.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('C:/Users/vhmir/PycharmProjects/pythonProject/WebAutomation/chromedriver.exe')
+driver.get("https://www.python.org")
+print(driver.title)
+search_bar.clear()
+search_bar.send_keys("getting started with python")
+search_bar.send_keys(Keys.RETURN)
+print(driver.current_url)
+driver.close()
