72de747330fa9af2cd3e7740d6888bfc2c8bcf83
Vikaus Hassan Mir <mirvakas@gmail.com>
2023-02-12 15:30:41 +0530

    2/12/2023 - making the code public

--- Business Analytics/Spend Data Active Users.py
+++ Business Analytics/Spend Data Active Users.py
@@ -15,14 +15,14 @@ TimeStamp = str(year) + str(month) + str(day)
 # TimeStamp = datetime.date.today()
 
 cx_Oracle.init_oracle_client(lib_dir=r"C:/instantclient_19_12")
-userpwd = "alphabetagamma8"  # Obtain password string from a user prompt or environment variable
-dsn = cx_Oracle.makedsn("repos8.gdiindia.com", 1521, service_name="repos8.birchstreet.net")
+userpwd = "enter your password"  # Obtain password string from a user prompt or environment variable
+dsn = cx_Oracle.makedsn("host name", 1521, service_name="SERVICE_NAME")
 
-connection = cx_Oracle.connect(user="vhmir", password=userpwd, dsn=dsn, encoding="UTF-8")
+connection = cx_Oracle.connect(user="DBusername", password=userpwd, dsn=dsn, encoding="UTF-8")
 cur = connection.cursor()
-Total_Active_Users = "SELECT count(distinct(LOGINNAME)) as \"User Count\" FROM PBEACH_REPOS_B.LOGINHISTORY_SYN WHERE SUBSCRIBER_ID=641 and LOGINDATE>TO_DATE('2021-01-01', 'YYYY-MM-DD') and loginname not like 'bss_%'"
+Total_Active_Users = "SELECT count(distinct(LOGIN_NAME)) as \"User Count\" FROM DBSCHEMA.LOGIN_HISTORY WHERE SUB_ID=@CUST_ID and LOGINDATE>TO_DATE('2021-01-01', 'YYYY-MM-DD') and LOGIN_NAME not like 'SUPERUSER_%'"
 
-FileNameExcel = 'AmkAnalysis_UserData' + str(TimeStamp) + '.xlsx'
+FileNameExcel = 'Analysis_UserData' + str(TimeStamp) + '.xlsx'
 df2 = pd.read_sql(Total_Active_Users, connection)
 
 with pd.ExcelWriter(FileNameExcel) as writer:  # doctest: +SKIP
@@ -31,11 +31,11 @@ with pd.ExcelWriter(FileNameExcel) as writer:  # doctest: +SKIP
 connection.close()
 
 msg = EmailMessage()
-msg['Subject'] = 'Aramark Active Users Count'
-msg['From'] = 'vakasmir@yahoo.com'
-msg['To'] = 'abtiwari@birchstreet.net,vhmir@birchstreet.net'
+msg['Subject'] = 'Customer Active Users Count'
+msg['From'] = 'username@domain.com'
+msg['To'] = 'abc@domain.com,abc@domain.com'
 msg.set_content(
-    "Attached file contains the number of active Users.\nReach out to Vikaus for further clarity on data.\n")
+    "Attached file contains the number of active Users.\nReach out to me for further clarity on data.\n")
 
 with open(FileNameExcel, "rb") as f:
     file_data = f.read()
@@ -44,7 +44,7 @@ with open(FileNameExcel, "rb") as f:
     msg.add_attachment(file_data, maintype="application", subtype="xlsx", filename=file_name)
 try:
     server = smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465)
-    server.login("vakasmir@yahoo.com", "uorxxuctvgepoyso")
+    server.login("username@domain.com", "uorxxuctvgepoyso")
     server.send_message(msg)
     print("Email Sent Sucessfully!")
 except:
--- Business Analytics/Spend Data Till Date.py
+++ Business Analytics/Spend Data Till Date.py
@@ -13,16 +13,16 @@ TimeStamp = str(year)+str(month)+str(day)
 # TimeStamp = datetime.date.today()
 
 cx_Oracle.init_oracle_client(lib_dir=r"C:/instantclient_19_12")
-userpwd = "alphabetagamma7" # Obtain password string from a user prompt or environment variable
-dsn = cx_Oracle.makedsn("amer8.corp.birchstreet.net", 1521, service_name="AMER8")
+userpwd = "DBPwd" # Obtain password string from a user prompt or environment variable
+dsn = cx_Oracle.makedsn("HOSTNAME", PORT, service_name="SERVICE NAME")
 
-connection = cx_Oracle.connect(user="vhmir", password=userpwd, dsn=dsn,encoding="UTF-8")
+connection = cx_Oracle.connect(user="DBUserName", password=userpwd, dsn=dsn,encoding="UTF-8")
 cur = connection.cursor()
-Total_Live_Purchasing_Locations = "SELECT count(distinct a.company_Id) as \"A\" FROM PBEACH.PPO_MASTER_HEADER a,PBEACH.psm_company_profile b WHERE a.SUBSCRIBER_ID=641 and a.creation_date between TO_DATE('2021-01-01', 'YYYY-MM-DD') and TO_DATE('2022-12-01', 'YYYY-MM-DD') and nvl(a.is_deleted,0)!='1' and a.company_id not in (SELECT COMPANY_ID FROM PBEACH.PSM_COMPANY_PROFILE WHERE SUBSCRIBER_ID=641 and is_buyer=1 and active=1 and BATTR_VAL15 is null) and a.subscriber_id=b.subscriber_id and a.company_id=b.company_id and B.IS_BUYER=1 and b.active=1 and b.BATTR_VAL14 is not null"
+Total_Live_Purchasing_Locations = "SELECT count(distinct a.COMP_ID) as \"A\" FROM PO_HEADER a,COMP_PRFLE b WHERE a.SUB_ID=@SUBID and a.GEN_DATE between TO_DATE('2021-01-01', 'YYYY-MM-DD') and TO_DATE('2022-12-01', 'YYYY-MM-DD') and nvl(a.is_deleted,0)!='1' and a.COMP_ID not in (SELECT COMP_ID FROM COMP_PRFLE WHERE SUB_ID=@SUBID and is_buyer=1 and active=1) and a.SUB_ID=b.SUB_ID and a.COMP_ID=b.COMP_ID and B.IS_BUYER=1 and b.active=1 and b.ATTR is not null"
 
 # dfe = cur.execute(query1)
 # rows = cur.fetchall()
-FileNameExcel = 'AmkAnalysis_' + str(TimeStamp) + '.xlsx'
+FileNameExcel = 'SpendAnalysis_' + str(TimeStamp) + '.xlsx'
 df2 = pd.read_sql(Total_Live_Purchasing_Locations, connection)
 # with open('Spend Analysis.sql') as f:
 #     newText=f.read().replace('2022-08-01', '2022-08-01')
@@ -43,7 +43,7 @@ for line in Lines:
 df2.rename(columns={'A':'Number of Purchasing Locations Live','B':'Number of Invoicing Locations Live','C':'Total Credit Memos',
 'D':'Total Purchase Orders','E':'Total Invoices','F':'Total PO Lines','G':'Total AP Lines','H':'Total Spend in Purchase Orders','I':'Highest Value Purchase Order',
 'J':'Total Spend in AP Invoices','K':'Highest Value AP Invoice','L':'Highest Valued Line Item ordered','M':'Highest Valued AP Line','N':'Total Catalog items',
-'O':'Lowest Value Purchase Order','P':'PO Count Trend (8 Rows)','Q':'PO Spend Trend','R':'AP Count Trend','S':'AP Spend Trend','T':'BirchStreet Report Run Count',
+'O':'Lowest Value Purchase Order','P':'PO Count Trend (8 Rows)','Q':'PO Spend Trend','R':'AP Count Trend','S':'AP Spend Trend','T':'Report Run Count',
 'U':'PO Spend by Supplier (Last 15 Days)','V':'PO Spend by Supplier (Till Date)',
 'W':'Total Units','X1':'Count of Live Profit Centers','X':'Non PO Invoices', 'XA':'Feed Correction Non-PO Inv','Y':'Non PO Supplier Portal AP',
 'Z':'Non-PO EReceive','AB':'Manually Gen <>PO AP/Non-Elect',
@@ -54,24 +54,24 @@ df2.rename(columns={'A':'Number of Purchasing Locations Live','B':'Number of Inv
 
 with pd.ExcelWriter(FileNameExcel) as writer:  # doctest: +SKIP
     df2.to_excel(writer, sheet_name= 'Summary', index=False)
-    # df3.to_excel(writer, sheet_name= 'List of AP for Live PCs', index=False)
-    # df4.to_excel(writer, sheet_name= 'Feed Non PO Inv', index=False)
-    # df5.to_excel(writer, sheet_name= 'Feed PO Inv', index=False)
-    # df6.to_excel(writer, sheet_name= 'PO Sup Portal Inv', index=False)
-    # df7.to_excel(writer, sheet_name= 'NonPO Sup Portal Inv', index=False)
-    # df8.to_excel(writer, sheet_name= 'PO eReceiveInvoices', index=False)
-    # df9.to_excel(writer, sheet_name= 'Non PO eReceiveInvoices', index=False)
-    # df10.to_excel(writer, sheet_name= 'Manually Gen PO Inv', index=False)
-    # df11.to_excel(writer, sheet_name= 'Manually Gen Non PO Inv', index=False)
-    # df12.to_excel(writer, sheet_name= 'SQL STATEMENTS', index=False)
+    df3.to_excel(writer, sheet_name= 'List of AP for Live PCs', index=False)
+    df4.to_excel(writer, sheet_name= 'Feed Non PO Inv', index=False)
+    df5.to_excel(writer, sheet_name= 'Feed PO Inv', index=False)
+    df6.to_excel(writer, sheet_name= 'PO Sup Portal Inv', index=False)
+    df7.to_excel(writer, sheet_name= 'NonPO Sup Portal Inv', index=False)
+    df8.to_excel(writer, sheet_name= 'PO eReceiveInvoices', index=False)
+    df9.to_excel(writer, sheet_name= 'Non PO eReceiveInvoices', index=False)
+    df10.to_excel(writer, sheet_name= 'Manually Gen PO Inv', index=False)
+    df11.to_excel(writer, sheet_name= 'Manually Gen Non PO Inv', index=False)
+    df12.to_excel(writer, sheet_name= 'SQL STATEMENTS', index=False)
 
 connection.close()
 
 msg = EmailMessage()
-msg['Subject'] = 'Aramark Spend Data'
-msg['From'] = 'vakasmir@yahoo.com'
-msg['To'] = 'abtiwari@birchstreet.net,vhmir@birchstreet.net'
-msg.set_content("Attached file contains the details of Spend Data which includes the count of active locations/sites/Profit Centers.\nReach out to Vikaus for further clarity on data.")
+msg['Subject'] = 'Customer Spend Data'
+msg['From'] = 'username@domain.com'
+msg['To'] = 'username@domain.net,username2@domain.net'
+msg.set_content("Attached file contains the details of Spend Data which includes the count of active locations/sites/Profit Centers.\nReach out to me for further clarity on data.")
 
 with open(FileNameExcel, "rb") as f:
     file_data = f.read()
@@ -80,7 +80,7 @@ with open(FileNameExcel, "rb") as f:
     msg.add_attachment(file_data,maintype="application", subtype= "xlsx", filename = file_name)
 try:
     server = smtplib.SMTP_SSL('smtp.mail.yahoo.com', 465)
-    server.login("vakasmir@yahoo.com", "uorxxuctvgepoyso")
+    server.login("user@domain.com", "pwd")
     server.send_message(msg)
     print("Email Sent Sucessfully!")
 except:
